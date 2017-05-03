7b7264ae83db82824c174d8fea749cd8a03e7273
李强 <liqiang@bingosoft.net>
2017-05-03 16:56:22 +0800

    orm demo

---
+++ src/main/java/leap/api/controller/UserModelController.java
@@ -0,0 +1,44 @@
+package leap.api.controller;
+
+import leap.core.exception.RecordNotSavedException;
+import leap.demo.model.User;
+import leap.orm.query.CriteriaQuery;
+
+import java.util.List;
+
+/**
+ * Created by liqiang on 2017/5/3.
+ */
+public class UserModelController {
+    public User create(String name, Integer age, String loginId, String password) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        user.setLoginId(loginId);
+        user.setPassword(password);
+
+        try {
+            user.create();
+            return user;
+        } catch (RecordNotSavedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<User> query(String name, Integer age, String loginId){
+        if(name == null && age == null && loginId == null){
+            return User.all();
+        }
+        if(name == null){
+            name = "";
+        }
+        if(loginId == null){
+            loginId = "";
+        }
+        CriteriaQuery<User> cq = User.<User>query();
+        cq.where("name like ? and age like ? and loginId like ?",
+                "%"+name+"%",age==null?"%%":"%"+age+"%","%"+loginId+"%");
+        return cq.list();
+    }
+}
--- src/main/java/leap/demo/controller/HomeController.java
+++ src/main/java/leap/demo/controller/HomeController.java
@@ -6,6 +6,5 @@ import leap.web.view.ViewData;
 public class HomeController {
     public void index(ViewData vd, String name){
         vd.put("name", name);
-
     }
 }
